ca3021fe2683330b1485d852fb604b4d0bd04d6d
Takacs Olga <tackyolgi@gmail.com>
2020-08-24 08:31:18 +0200

    save tiff-s in 300 dpi

--- all process.R
+++ all process.R
@@ -21,16 +21,18 @@ source("./01_codes/02_estimations/mse.R")
 
 mse$Database <- factor(mse$Database, levels=c("Training sample",
                                               "Test sample"))
+library(tidyverse)
 
-tiff(as.character("./results/figures/fig1.tiff"))
+tiff(as.character("./results/figures/fig1.tiff"), 
+     width = 1750, height=1111, res=300)
 ggplot(subset(mse, mse$Gender=="Females"), aes(x=Year, y=as.numeric(MSE), fill=Method))+
   geom_bar(stat = "identity", width = 0.5)+
   facet_grid(Database~Method)+labs(x="", y="")+
   theme(legend.position = "none")+
-  theme(strip.text.x = element_text(size=12),
-        strip.text.y = element_text(size=12),
-        axis.text.x = element_text(size=12),
-        axis.text.y = element_text(size=12))
+  theme(strip.text.x = element_text(size=8),
+        strip.text.y = element_text(size=8),
+        axis.text.x = element_text(size=8),
+        axis.text.y = element_text(size=8))
 dev.off()
 
 # table 3 and table 4 
@@ -54,11 +56,13 @@ results_RF_longer$dataset <- factor(results_RF_longer$dataset,
                                     c("training", "test"))
 panel_label <- c(training="A", test="B")
 
-tiff("./results/figures/Fig2.tiff")
+tiff("./results/figures/Fig2.tiff", 
+     width = 1900, height=1111, res=300)
 ggplot(results_RF_longer, aes(x=year, y=value, fill=effects))+
   geom_bar(position="dodge", stat="identity")+
   scale_y_continuous(breaks=seq(-40, 140, 20))+
-  facet_grid(.~dataset, labeller=labeller(dataset=panel_label))+ theme(legend.position = "bottom")+
+  facet_grid(.~dataset, labeller=labeller(dataset=panel_label))+ 
+  theme(legend.position = "bottom")+
   xlab("")+ylab("")+
   theme(legend.title = element_blank())+
   theme(strip.background = element_rect(fill="white"))
@@ -71,7 +75,8 @@ results_RF_longer <- results_RF %>% pivot_longer(cols=c("wage_structure_effect",
 results_RF_longer$dataset <- factor(results_RF_longer$dataset,
                                     c("training", "test"))
 
-tiff("./results/figures/Fig3.tiff")
+tiff("./results/figures/Fig3.tiff",
+     width = 1750, height=1111, res=300)
 ggplot(results_RF_longer, aes(x=year, y=value, color=raw_ws))+
   geom_line()+
   facet_grid(.~dataset, labeller=labeller(dataset=panel_label))+ theme(legend.position = "bottom")+
@@ -87,7 +92,8 @@ source('./01_codes/04_ws effects/diff_osszehasonlitas_iskveg.R')
 
 pivot_iskveg4$dataset <- factor(pivot_iskveg4$dataset,
                                     c("training", "test"))
-tiff("./results/figures/Fig4.tiff")
+tiff("./results/figures/Fig4.tiff", 
+     width = 1200, height=1200, res=300)
 ggplot(pivot_iskveg4, aes(x=category, y=as.numeric(RF), group=year, color=year ))+
   geom_path()+facet_grid(.~dataset, labeller=labeller(dataset=panel_label))+ theme(legend.position = "bottom")+
   xlab("")+ylab("")+labs(fill="")+ylim(0,0.25)+
@@ -100,7 +106,8 @@ source('./01_codes/04_ws effects/diff_osszehasonlitas_kor.R')
 
 pivot_kor$dataset <- factor(pivot_kor$dataset,
                             c("training", "test"))
-tiff("./results/figures/Fig5.tiff")
+tiff("./results/figures/Fig5.tiff", 
+     width = 1200, height=1200, res=300)
 ggplot(pivot_kor, aes(x=category, y=as.numeric(RF), group=year, color=year ))+
   geom_path()+facet_grid(.~dataset, labeller=labeller(dataset=panel_label))+ theme(legend.position = "bottom")+
   xlab("")+ylab("")+labs(fill="")+ylim(0,0.25)+
@@ -113,7 +120,8 @@ source('./01_codes/04_ws effects/diff_osszehasonlitas_szolgho.R')
 
 pivot_szolgho$dataset <- factor(pivot_szolgho$dataset,
                             c("training", "test"))
-tiff("./results/figures/Fig6.tiff")
+tiff("./results/figures/Fig6.tiff", 
+     width = 1200, height=1200, res=300)
 ggplot(pivot_szolgho, aes(x=category, y=as.numeric(RF), group=year, color=year ))+
   geom_path()+facet_grid(.~dataset, labeller=labeller(dataset=panel_label))+ theme(legend.position = "bottom")+
   xlab("")+ylab("")+labs(fill="")+ylim(0,0.25)+
@@ -126,7 +134,8 @@ source('./01_codes/04_ws effects/diff_osszehasonlitas_kra_4.R')
   
 pivot_kra$dataset <- factor(pivot_kra$dataset,
                               c("training", "test"))
-tiff("./results/figures/Fig7.tiff")
+tiff("./results/figures/Fig7.tiff", 
+     width = 1200, height=1200, res=300)
 ggplot(pivot_kra, aes(x=category, y=as.numeric(RF), group=year, color=year ))+
   geom_path()+facet_grid(.~dataset, labeller=labeller(dataset=panel_label))+ theme(legend.position = "bottom")+
   xlab("")+ylab("")+labs(fill="")+ylim(0,0.25)+
@@ -140,7 +149,8 @@ source('./01_codes/04_ws effects/diff_osszehasonlitas_ara_4.R')
   
 pivot_ara$dataset <- factor(pivot_ara$dataset,
                                 c("training", "test"))
-tiff("./results/figures/Fig8.tiff")
+tiff("./results/figures/Fig8.tiff", 
+     width = 1200, height=1200, res=300)
 ggplot(pivot_ara, aes(x=category, y=as.numeric(RF), group=year, color=year ))+
   geom_path()+facet_grid(.~dataset, labeller=labeller(dataset=panel_label))+ theme(legend.position = "bottom")+
   xlab("")+ylab("")+labs(fill="")+ylim(0,0.25)+
@@ -155,7 +165,8 @@ source('./01_codes/04_ws effects/diff_osszehasonlitas_nok_aranya.R')
 pivot_nok_aranya$dataset <- factor(pivot_nok_aranya$dataset,
                                 c("training", "test"))
 
-tiff("./results/figures/Fig9.tiff")
+tiff("./results/figures/Fig9.tiff", 
+     width = 1200, height=1200, res=300)
 ggplot(pivot_nok_aranya, aes(x=category, y=as.numeric(RF), group=year, color=year ))+
   geom_path()+facet_grid(.~dataset, labeller=labeller(dataset=panel_label))+ theme(legend.position = "bottom")+
   xlab("")+ylab("")+labs(fill="")+ylim(0,0.25)+
@@ -169,7 +180,8 @@ source('./01_codes/04_ws effects/diff_osszehasonlitas_feor.R')
 pivot_feor$dataset <- factor(pivot_feor$dataset,
                              c("training", "test"))
 
-tiff("./results/figures/Fig10.tiff")
+tiff("./results/figures/Fig10.tiff", 
+     width = 1200, height=1200, res=300)
 ggplot(pivot_feor, aes(x=category, y=as.numeric(RF), group=year, color=year ))+
   geom_path()+facet_grid(.~dataset, labeller=labeller(dataset=panel_label))+ theme(legend.position = "bottom")+
   xlab("")+ylab("")+labs(fill="")+ylim(0,0.25)+
@@ -178,7 +190,7 @@ ggplot(pivot_feor, aes(x=category, y=as.numeric(RF), group=year, color=year ))+
 dev.off()
 
 
-# H* and L* characteristics - table 8-10
+# L* and S* characteristics - table 8-10
 source("./01_codes/03_carts/CARTs_groups.R")
 
 # leave out some variables
@@ -205,10 +217,13 @@ unemployment <- get_eurostat("une_rt_a", time_format = "num",
                                           age="Y15-74", sex="T", unit="PC_ACT"))
 unemp_job_vac <- left_join(unemployment, job_vacancy, by=c("time"= "time"))
 
-tiff("./results/figures/Fig11.tiff")
+tiff("./results/figures/Fig11.tiff", 
+     width = 1200, height=1200, res=300)
 ggplot(unemp_job_vac, aes(x=values.x, y=values.y, label=time))+
-  geom_path()+ylim(0,2)+xlim(0,12)+geom_text(size=3)+
-  xlab("")+ylab("")+theme(legend.title = element_blank())
+  geom_path()+ylim(0,2)+xlim(0,12)+
+  geom_text(size=3)+
+  xlab("")+ylab("")+
+  theme(legend.title = element_blank())
 dev.off()
 
 # S1 figure
@@ -248,7 +263,8 @@ results_RF_longer$year <- as.character(results_RF_longer$year)
 results_RF_longer$dataset <- factor(results_RF_longer$dataset,
                                     c("training", "test"))
 
-tiff("./results/figures/S1_fig.tiff")
+tiff("./results/figures/S1_fig.tiff", 
+     width = 1200, height=1200, res=300)
 ggplot(results_RF_longer, aes(x=year, y=value, fill=effects))+
   geom_bar(position="dodge", stat="identity")+
   scale_y_continuous(breaks=seq(-40, 140, 20))+
@@ -290,3 +306,213 @@ rmarkdown::render("./01_codes/03_carts/CARTs_2016.R",
 setwd("C:/Users/tacky/OneDrive - Corvinus University of Budapest/phd/research/technical note/BO decomp")
 
 # further calculations in the excel file
+
+# S? - obb error tables
+# 2008
+model <- readRDS("./models/B_RF_female_ordered_2008.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_female_2008.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+model <- readRDS("./models/B_RF_male_ordered_2008.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_male_2008.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+# 2009
+model <- readRDS("./models/B_RF_female_ordered_2009.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_female_2009.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+model <- readRDS("./models/B_RF_male_ordered_2009.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_male_2009.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+# 2010
+model <- readRDS("./models/B_RF_female_ordered_2010.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_female_2010.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+model <- readRDS("./models/B_RF_male_ordered_2010.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_male_2010.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+# 2011
+model <- readRDS("./models/B_RF_female_ordered_2011.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_female_2011.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+model <- readRDS("./models/B_RF_male_ordered_2011.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_male_2011.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+# 2012
+model <- readRDS("./models/B_RF_female_ordered_2012.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_female_2012.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+model <- readRDS("./models/B_RF_male_ordered_2012.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_male_2012.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+# 2013
+model <- readRDS("./models/B_RF_female_ordered_2013.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_female_2013.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+model <- readRDS("./models/B_RF_male_ordered_2013.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_male_2013.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+# 2014
+model <- readRDS("./models/B_RF_female_ordered_2014.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_female_2014.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+model <- readRDS("./models/B_RF_male_ordered_2014.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_male_2014.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+# 2015
+model <- readRDS("./models/B_RF_female_ordered_2015.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_female_2015.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+model <- readRDS("./models/B_RF_male_ordered_2015.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_male_2015.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+# 2016
+model <- readRDS("./models/B_RF_female_ordered_2016.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_female_2016.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+model <- readRDS("./models/B_RF_male_ordered_2016.RData")
+
+error_rate <- as.data.frame(model$err.rate)
+
+tiff(as.character("./results/figures/error_rate_male_2016.tiff"), 
+     width = 1200, height=1200, res=300)
+ggplot(error_rate, aes(y=model$err.rate, x=seq(1:1000)))+
+  geom_line()+ labs(x="", y="")+
+  theme(axis.title.x = element_text(size=12))
+dev.off()
+
+
